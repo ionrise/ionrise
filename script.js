5546ccb2e3f0a783c367ad0440082fe7c2a6187e
ionrise <140621287+ionrise@users.noreply.github.com>
2025-02-17 02:12:39 +0100

    Update script.js
    delete

--- script.js
+++
@@ -1,9 +0,0 @@
-var meta = document.querySelector('meta[name="Ionrise One"]');
-    if (meta) {
-        meta.setAttribute('content', 'This is the official page of Ionrise. We´re launching soon!');
-    } else {
-    meta = document.createElement('meta');
-    meta.name = 'Ionrise One';
-    meta.content = 'This is the official page of Ionrise. We´re launching soon!';
-    document.getElementsByTagName('head')[0].appendChild(meta);
-}
